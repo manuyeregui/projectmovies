4a0c990f1d809266d95dfd91a92a858cd6b3adf9
Manuel Yeregui <manuelyeregui@gmail.com>
2022-08-12 20:06:27 -0300

    adding fullscreen backdrop loading

--- src/components/FullScreenMovies.js
+++ src/components/FullScreenMovies.js
@@ -7,8 +7,6 @@ function FullScreenMovies(props) {
     const [moviesList, setMoviesList] = useState([]);
     const [loadingMovie, setLoadingMovie] = useState(true);
 
-    const [moviesListLength, setMoviesListLength] = useState(null);
-
     const [index, setIndex] = useState(0);
 
     /*const [movie, setMovie] = useState(null);*/
@@ -19,8 +17,7 @@ function FullScreenMovies(props) {
         let data = await rawData.json()
 
         setLoadingMovie(false)
-        setMoviesListLength(data.results.length)
-        setMoviesList(data.results)
+        setMoviesList(data.results.filter(m => m.popularity > 5 && m.original_language !== 'hi'))
     }
 
     useEffect(() => {
@@ -57,6 +54,13 @@ function FullScreenMovies(props) {
         
     }, [])
 
+    const [isBackdropLoaded, setIsBackdropLoaded] = useState(false);
+
+    const changeLoading = () => {
+        setTimeout(() => {setIsBackdropLoaded(true)}, 100)
+        
+    }
+
     return (
         !loadingMovie &&
         <div className='homepage-fullscreen-box'>
@@ -66,68 +70,38 @@ function FullScreenMovies(props) {
                 animate={{ opacity: 1, x: 0, transition: {duration: 0.2} }}
                 exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
                 key={moviesList[index].backdrop_path}
-                className='full-screen-img' src={'https://image.tmdb.org/t/p/original/' + moviesList[index].backdrop_path}
+                className={!isBackdropLoaded ? 'full-screen-img loading-movie' : 'full-screen-img loading-movie loaded'}
+                src={'https://image.tmdb.org/t/p/w300/' + moviesList[index].backdrop_path}
+            />
+
+            <motion.img
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0, transition: {duration: 0.2} }}
+                exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
+                key={'loading' + moviesList[index].backdrop_path}
+                className='full-screen-img'
+                loading='lazy'
+                src={'https://image.tmdb.org/t/p/original/' + moviesList[index].backdrop_path}
+                onLoad={changeLoading}
             />
             
             <div className='gradient'>
                 <div className='fullscreen-content-box' ref={content}>
                     <div className='inner-fullscreen-content-box'>
 
-                        <div>
-                            <motion.span
-                                onClick={previousIndex}
-                                className={index === 0 ? "material-symbols-rounded switchmovie-inactive" : "material-symbols-rounded switchmovie-active"}
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1, transition: {duration: 0.2} }}
-                                exit={{ opacity: 0, transition: {duration: 0.2} }}
-                                key={'next' + index}
-                            >
-                                arrow_back_ios
-                            </motion.span>
-                            <motion.span
-                                onClick={nextIndex}
-                                className={index + 1 === moviesList.length ? "material-symbols-rounded switchmovie-inactive" : "material-symbols-rounded switchmovie-active"}
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1, transition: {duration: 0.2} }}
-                                exit={{ opacity: 0, transition: {duration: 0.2} }}
-                                key={'back' + index}
-                            >
-                                arrow_forward_ios
-                            </motion.span>
-                        </div>
-
-                        <motion.h2
-                            initial={{ opacity: 0, x: 100 }}
-                            animate={{ opacity: 1, x: 0, transition: {duration: 0.2, delay: 0.1} }}
-                            exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
-                            key={moviesList[index].title}
-                        >
-                            {moviesList[index].title}
-                        </motion.h2>
-
                         {moviesList[index].tagline &&
                         <motion.h6
                             initial={{ opacity: 0, x: 100 }}
-                            animate={{ opacity: 1, x: 0, transition: {duration: 0.2, delay: 0.2} }}
+                            animate={{ opacity: 1, x: 0, transition: {duration: 0.2, delay: 0.4} }}
                             exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
                             key={moviesList[index].tagline}
                         >
                             {moviesList[index].tagline}
                         </motion.h6>}
 
-                        <motion.p
-                            initial={{ opacity: 0, x: 100 }}
-                            animate={{ opacity: 1, x: 0, transition: {duration: 0.2, delay: 0.3} }}
-                            exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
-                            className={showOverview ? null : 'display-none'}
-                            key={moviesList[index].overview}
-                        >
-                            {moviesList[index].overview}
-                        </motion.p>
-
                         <motion.div
                             initial={{ opacity: 0, x: 100 }}
-                            animate={{ opacity: 1, x: 0, transition: {duration: 0.2, delay: 0.4} }}
+                            animate={{ opacity: 1, x: 0, transition: {duration: 0.2, delay: 0.3} }}
                             exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
                             key={'/movies/' + moviesList[index].id}
                         >
@@ -141,6 +115,42 @@ function FullScreenMovies(props) {
                                 </span>
                             </Link>
                         </motion.div>
+
+                        <motion.h2
+                            initial={{ opacity: 0, x: 100 }}
+                            animate={{ opacity: 1, x: 0, transition: {duration: 0.2, delay: 0.2} }}
+                            exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
+                            key={moviesList[index].title}
+                        >
+                            {moviesList[index].title}
+                        </motion.h2>
+
+                        <motion.p
+                            initial={{ opacity: 0, x: 100 }}
+                            animate={{ opacity: 1, x: 0, transition: {duration: 0.2, delay: 0.1} }}
+                            exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
+                            className={showOverview ? null : 'display-none'}
+                            key={moviesList[index].overview}
+                        >
+                            {moviesList[index].overview}
+                        </motion.p>
+
+                        <h3>{props.title}</h3>
+
+                        <div>
+                            <span
+                                onClick={previousIndex}
+                                className={index === 0 ? "material-symbols-rounded switchmovie-inactive" : "material-symbols-rounded switchmovie-active"}
+                            >
+                                arrow_back_ios
+                            </span>
+                            <span
+                                onClick={nextIndex}
+                                className={index + 1 === moviesList.length ? "material-symbols-rounded switchmovie-inactive" : "material-symbols-rounded switchmovie-active"}
+                            >
+                                arrow_forward_ios
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,15 +7,15 @@ function Homepage() {
 
   return (
     <main>
-      <FullScreenMovies api={'https://api.themoviedb.org/3/movie/popular?api_key=' + process.env.REACT_APP_TMDB_KEY + '&language=en-US&page=1'}/>
+      <FullScreenMovies title='Popular right now.' api={'https://api.themoviedb.org/3/movie/popular?api_key=' + process.env.REACT_APP_TMDB_KEY + '&language=en-US&page=1'}/>
       <motion.div id="wrapper"
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x:-100, transition: {duration: 0.2} }}
       >
+          {/*<MoviesWrapper title="POPULAR RIGHT NOW." api={'https://api.themoviedb.org/3/movie/popular?api_key=' + process.env.REACT_APP_TMDB_KEY + '&language=en-US&page=1'} showRating={true}/>*/}
           <MoviesWrapper title="TODAY TRENDING." api={'https://api.themoviedb.org/3/trending/movie/day?api_key=' + process.env.REACT_APP_TMDB_KEY} showRating={false}/>
           <MoviesWrapper title="TOP RATED." api={'https://api.themoviedb.org/3/movie/top_rated?api_key=' + process.env.REACT_APP_TMDB_KEY + '&language=en-US&page=1'} showRating={true}/>
-          <MoviesWrapper title="POPULAR RIGHT NOW." api={'https://api.themoviedb.org/3/movie/popular?api_key=' + process.env.REACT_APP_TMDB_KEY + '&language=en-US&page=1'} showRating={true}/>
           <MoviesWrapper title="UPCOMING." api={'https://api.themoviedb.org/3/movie/upcoming?api_key=' + process.env.REACT_APP_TMDB_KEY + '&language=en-US&page=1'} showRating={false}/>
       </motion.div>
     </main>
